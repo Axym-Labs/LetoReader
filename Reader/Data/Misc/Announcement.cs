b99d1d0c8ae972281a9a90ec3b7dde2324d59e0d
Davide R. Wiest <davide.wiest2@gmail.com>
2024-04-28 15:55:43 +0200

    add share buttons, add announcement features

---
+++ Reader/Data/Misc/Announcement.cs
@@ -0,0 +1,40 @@
+﻿namespace Reader.Data.Misc;
+
+public class Announcement
+{
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+    public int ShowForDays { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public bool Priority { get; set; }
+    public AnnouncementAction Action { get; set; }
+
+    public Announcement(int id, DateTime date, int showForDays, string title, string description, bool priority, AnnouncementAction action)
+    {
+        Id = id;
+        Date = date;
+        ShowForDays = showForDays;
+        Title = title;
+        Description = description;
+        Priority = priority;
+        Action = action;
+    }
+
+    public bool IsExpired()
+    {
+        return Date.AddDays(ShowForDays) < DateTime.Now;
+    }
+}
+
+public class AnnouncementAction
+{
+    public string Url { get; set; }
+    public string Title { get; set; }
+
+    public AnnouncementAction(string url, string title)
+    {
+        Url = url;
+        Title = title;
+    }
+}
--- Reader/Data/Storage/Constants.cs
+++ Reader/Data/Storage/Constants.cs
@@ -7,6 +7,6 @@ public class Constants
     public const string ContentDir = "/static/content";
     public int CurrentYear = DateTime.Now.Year;
 
-    public const string CentralLoggerEndpoint = "http://127.0.0.1:5000/log"; //"https://exym-log.davidewiest.com/log";
-    public const string CentralAnnouncementsEndpoint = "http://127.0.0.1:5000/announcements.json";
+    public const string CentralLoggerEndpoint = "https://axym-con.davidewiest.com/log";
+    public const string CentralAnnouncementsEndpoint = "https://axym-con.davidewiest.com/announcements.json";
 }
