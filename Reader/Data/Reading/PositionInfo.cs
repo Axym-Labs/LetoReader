1aefdc5fece8de51cbe610fb5d0e785e127cde05
Jonathan Schäfer <jonathan.schaefer_@outlook.de>
2024-05-20 17:08:06 +0200

    Changes

--- Reader/Data/Reading/PositionInfo.cs
+++ Reader/Data/Reading/PositionInfo.cs
@@ -1,13 +1,20 @@
-﻿namespace Reader.Data.Reading;
+﻿using Reader.Modules;
+
+namespace Reader.Data.Reading;
 
 public class PositionInfo
 {
     public PositionInfo(PositionalMethod positionalMethod, string text)
     {
-
+        MaxPosition = positionalMethod switch
+        {
+            PositionalMethod.Word => (uint)TextHelper.SeparateText(TextHelper.Sanitize(text)).Count(),
+            PositionalMethod.CharIndex => throw new NotImplementedException("Implement logic for splitting using CharIndex as positioning method"),
+            _ => throw new NotImplementedException()
+        };
     }
 
-    public uint Position { get; set; }
+    public int Position { get; set; }
     public PositionalMethod PositionalMethod { get; set; }
     public uint MaxPosition { get; set; }
 }
